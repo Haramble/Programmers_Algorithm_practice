8d574ddc462f30dde0645b4c4d007bc76d49fa67
Haramble <43665682+Haramble@users.noreply.github.com>
2019-03-29 19:46:09 +0900

    Update 가장 먼 노드 (그래프).cc

--- 가장 먼 노드 (그래프).cc
+++ 가장 먼 노드 (그래프).cc
@@ -9,21 +9,25 @@ int solution(int n, vector<vector<int>> edge) {
     int answer[n+1], index=0;
     vector<int> map[n+1];
     bool visit[n+1];
+    //초기화 부분
     for(int i=0; i<n+1; i++){
         answer[i] = 0;
         visit[i] = false;
     }
+    //간선들로 map을 만듬.
     for(int i=0; i<edge.size(); i++){
         map[edge[i][0]].push_back(edge[i][1]);
         map[edge[i][1]].push_back(edge[i][0]);
     }
-    queue<pair<int, int> > q;
+    
+    //bfs
+    queue<pair<int, int> > q; //queue에 pair값을 넣는데 first는 노드의 번호, second는 간선을 몇번 거쳐서 왔는지 나타낸다.
     q.push({1,0});
     visit[1] = true;
     while(!q.empty()){
         int cur = q.front().first;
         int eg = q.front().second;
-        if(index < eg) index = eg;
+        if(index < eg) index = eg; //최대 간선길이 갱신
         q.pop();
 
         for(vector<int>::iterator it=map[cur].begin(); it<map[cur].end(); it++){
